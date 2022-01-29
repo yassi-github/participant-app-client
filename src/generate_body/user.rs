ca7230d1fb9000735367472ef1ef1b613edf56fc
yassi-github <s19t312@kagawa-u.ac.jp>
2022-01-29 12:18:14 +0000

    :art: chg content-type to json

--- src/generate_body/user.rs
+++ src/generate_body/user.rs
@@ -1,6 +1,5 @@
 use super::mac;
 use crate::userdata;
-use form_urlencoded;
 
 /// generate body.
 /// this uses mac addr data from mac.rs
@@ -12,12 +11,12 @@ pub fn generate_request_body() -> Result<String, Box<dyn std::error::Error>> {
     let name = readed_data.user.name;
 
     // sample POST body:
-    // id=19T999&name=kagawataro&macaddress=aa%3Aaa%3Aaa%3Aaa%3Aaa%3Aaa
-    let request_body = form_urlencoded::Serializer::new(String::new())
-        .append_pair("id", &id)
-        .append_pair("name", &name)
-        .append_pair("macaddress", &mac_data)
-        .finish();
+    // {"id":"19T999","name":"test-name","macaddress":"3c:06:30:43:3f:52"}
+    let request_body = serde_json::json!({
+        "id": id,
+        "name": name,
+        "macaddress": mac_data,
+    }).to_string();
 
 
     Ok(request_body)
